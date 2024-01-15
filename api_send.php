a66d97b5d6921872674081bbdcd37d82ae1f3839
shivani5634 <shivanigupta5634769@gmail.com>
2024-01-15 23:38:54 +0530

    table add

--- api_send.php
+++ api_send.php
@@ -6,7 +6,7 @@ curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 $result=curl_exec($ch);
 curl_close($ch);
 $result=json_decode($result,true);
-// echo $result;
+// echo $result;die();
 if(isset($result['status']))
 {
     
@@ -14,10 +14,24 @@ if(isset($result['status']))
         
         if(isset($result['result'])){
            
-            if($result['result']==true){
-                echo "<pre>";
-                print_r($result['data']);
-            }
+            if($result['result']==true){?>
+                <!-- // echo "<pre>";
+                // print_r($result['data']); -->
+           <table class="table table-light">
+            <tbody>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Email</td>
+                </tr>
+                <?php foreach($result['data'] as $item){?>
+<tr><td><?php echo $item['id']?></td>
+<td><?php echo $item['name']?></td>
+<td><?php echo $item['email']?></td></tr>
+                    <?php }?>
+            </tbody>
+           </table>
+            <?php }
         }
     }
 }
