81ba408745571404c788f6f6d9e1da5bfaed76a3
shivani5634 <shivanigupta5634769@gmail.com>
2024-01-28 23:21:09 +0530

    token authentication

--- api_send.php
+++ api_send.php
@@ -1,5 +1,5 @@
 <?php
-$url="http://localhost/phplogin/index.php";
+$url="http://localhost/phplogin/index.php?key=gfhfhfghtyutyutyasedawerwrwerteww";
 $ch=curl_init();
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
--- index.php
+++ index.php
@@ -1,28 +1,37 @@
 <?php
 include('db.php');
-$sql='SELECT * FROM users_info';
-$res=mysqli_query($conn,$sql);
-$count=mysqli_num_rows($res);
-// $result=mysqli_fetch_assoc($res);
+if(isset($_GET['key'])){
+    $key=mysqli_real_escape_string($conn,$_GET['key']);
+    $checkres=mysqli_query($conn,"select status from info where token='$key'");
 
-if($count>0){
-    // $result=mysqli_fetch_assoc($res);
-    while($row=mysqli_fetch_assoc($res)){
-     
-        $arr[]=$row;
+    if(mysqli_num_rows($checkres)>0){
+        $checkrow=mysqli_fetch_assoc($checkres);
+       if($checkrow['status']=='1'){
+        $sql='SELECT * FROM users_info';
+        $res=mysqli_query($conn,$sql);
+        $count=mysqli_num_rows($res);
+        
+        
+        if($count>0){
+            
+            while($row=mysqli_fetch_assoc($res)){
+                $arr[]=$row;
+            }
+           echo json_encode(['status'=>'true','data'=>$arr,'result'=>'found']);
+        
+        }
+        else{
+            echo json_encode(['status'=>'true','data'=>$arr,'result'=>'not found']);
+        }
+    }else{
+        echo json_encode(['status'=>'true','data'=>'please provide valid api']);
     }
-   echo json_encode(['status'=>'true','data'=>$arr,'result'=>'found']);
-// echo '<pre>';
-// print_r($arr);
-// echo '</pre>';
+       }
+    
+   
+}else{
+    echo json_encode(['status'=>'true','data'=>'key is not given']);
 }
-else{
-    echo json_encode(['status'=>'true','data'=>$arr,'result'=>'not found']);
-}
-// if($conn){
-//     echo "Connection successfully!";
-// }
-// else{
-//     echo "connection failed";
-// }
+
+
 ?>
