74b50367fcf87f64f6dca79e28b7367d99623f3c
shivani5634 <shivanigupta5634769@gmail.com>
2024-01-28 23:54:19 +0530

    hit_limit hit_count

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 include('db.php');
 if(isset($_GET['key'])){
     $key=mysqli_real_escape_string($conn,$_GET['key']);
-    $checkres=mysqli_query($conn,"select status from info where token='$key'");
+    $checkres=mysqli_query($conn,"select * from info where token='$key'");
 
     if(mysqli_num_rows($checkres)>0){
         $checkrow=mysqli_fetch_assoc($checkres);
        if($checkrow['status']=='1'){
+        if($checkrow['hit_limit']>$checkrow['hit_count']){
+            mysqli_query($conn,"update info set hit_count=hit_count+1 where token='$key'");
+           
+        }else{
+            echo json_encode(['status'=>'true','data'=>'hit count & hit limit exceeded']); die();
+        }
         $sql='SELECT * FROM users_info';
         $res=mysqli_query($conn,$sql);
         $count=mysqli_num_rows($res);
@@ -18,7 +24,7 @@ if(isset($_GET['key'])){
                 $arr[]=$row;
             }
            echo json_encode(['status'=>'true','data'=>$arr,'result'=>'found']);
-        
+           
         }
         else{
             echo json_encode(['status'=>'true','data'=>$arr,'result'=>'not found']);
